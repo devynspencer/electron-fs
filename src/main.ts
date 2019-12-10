8280319b0b0b9a4996e0d709ddb8057c19f46133
Devyn Spencer <devynspencer@users.noreply.github.com>
2019-12-10 13:04:09 -0800

    Increase default app size

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,8 @@ let mainWindow;
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1200,
+    height: 900,
     webPreferences: {
       nodeIntegration: true
     }
